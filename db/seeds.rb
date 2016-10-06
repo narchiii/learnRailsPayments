3f0df1f24eede40551680c583b367bac08db4ad1
narchi <learnappcode@gmail.com>
2016-10-06 00:26:36 -0400

    added first product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+niceCat = Product.create(title: "Quality Kitty", 
+ subtitle: "Learn App Code", image_name: "kitty.JPG",
+ price: "4.99", sku: "kittyOne", download_url: "https://s3-us-west-2.amazonaws.com/rails-learnapp-bucket/kitty.JPG", 
+ details: "A really nice cat", description: %{<p>It's a cat</p>
+ })
