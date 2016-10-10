73093de23fd2dcafc6503e9c6a01f5bdd0a1fe42
narchi <learnappcode@gmail.com>
2016-10-10 17:20:51 -0400

    added subscription plan for Cat

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,18 +6,12 @@ class ChargesController < ApplicationController
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
-	    :source  => params[:stripeToken]
-	  )
-
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => product.price_in_cents,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
+	    :source  => params[:stripeToken],
+	    :plan => "CAT"
 	  )
 
 	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
-	  	amount: product.price_in_cents, description: charge.description, currency: charge.currency,
+	  	amount: product.price_in_cents, description: product.description, currency: "usd",
 	  	customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
 	  redirect_to purchase
