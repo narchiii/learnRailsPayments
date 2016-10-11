def612122aee9155d82e8f9190d5ef4788d46804
narchi <learnappcode@gmail.com>
2016-10-10 23:51:14 -0400

    updated teh charges controller to charge for the right product

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,13 +1,13 @@
 class ChargesController < ApplicationController
 	
 	def create
-	  product = Product.find_by_sku("kittyOne")
+	  product = Product.find(params[:product_id])
 	  # Amount in cents
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
 	    :source  => params[:stripeToken],
-	    :plan => "CAT"
+	    :plan => product.sku
 	  )
 
 	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'products#index'
   resources :charges
   resources :purchases, only: [:show]
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
 end
